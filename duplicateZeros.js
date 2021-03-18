2b8334dd0502517f92bb959dd5f81d34a0404f41
keystrega55 <72213479+keystrega55@users.noreply.github.com>
2021-03-18 16:39:05 +0100

    Create duplicateZeros.js
    First pass (naive implementation)

---
+++ duplicateZeros.js
@@ -0,0 +1,13 @@
+// Time: O(n)^2
+// Space: O(1)
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] == 0) {
+                for (int j = arr.length - 1; j > i; j--)
+                    arr[j] = arr[j-1];
+                i++;
+            }
+        }
+    }
+}
