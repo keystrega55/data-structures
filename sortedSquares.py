e1be809e81271cfc6f237ca0dc6cee6431efc86b
keystrega55 <72213479+keystrega55@users.noreply.github.com>
2021-06-04 14:34:24 +0200

    Update sortedSquares.py

--- sortedSquares.py
+++ sortedSquares.py
@@ -1,28 +1,35 @@
 # Given an integer array nums sorted in increasing order, return an array of the squares of each number sorted in increasing order.
 # Time: O(n)
 # Space: O(n)
+# https://leetcode.com/explore/learn/card/fun-with-arrays/523/conclusion/3574/
 
-def sortedSquares(nums):
-    if len(nums) == 0:
-        return nums
+class Solution:
+    def sortedSquares2(self, nums: List[int]) -> List[int]:
+        return list(sorted([x*x for x in nums]))
 
-    num_list = list(nums)
-    sorted_squares = [None] * len(nums)
-    left_pointer = 0
-    right_pointer = len(nums) - 1
+    def sortedSquares(nums):
+        if len(nums) == 0:
+            return nums
 
-    for i, num in reversed(list(enumerate(nums))):
-        square = 0
-        if abs(nums[left_pointer]) < abs(nums[right_pointer]):
-            square = nums[right_pointer] * nums[right_pointer]
-            right_pointer -= 1
-        else:
-            square = nums[left_pointer] * nums[left_pointer]
-            left_pointer += 1
-        sorted_squares[i] = square
+        num_list = list(nums)
+        sorted_squares = [None] * len(nums)
+        left_pointer = 0
+        right_pointer = len(nums) - 1
 
-    return sorted_squares
+        for i, num in reversed(list(enumerate(nums))):
+            square = 0
+            if abs(nums[left_pointer]) < abs(nums[right_pointer]):
+                square = nums[right_pointer] * nums[right_pointer]
+                right_pointer -= 1
+            else:
+                square = nums[left_pointer] * nums[left_pointer]
+                left_pointer += 1
+            sorted_squares[i] = square
+
+        return sorted_squares
 
 
 nums = [-4, -1, 0, 3, 10]
-print(sortedSquares(nums))
+
+sol = Solution()
+print(sol.sortedSquares(nums))
